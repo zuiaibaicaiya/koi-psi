a18b0d62d78cabb8b80d832d15758b8de01ee8f5
hanyun <1355081829@qq.com>
2025-02-27 22:27:55 +0800

    init

---
+++ ui/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [vue()],
+  build: {
+    outDir: '../admin/ui',
+  }
+})
